30a9ac001e4e6f0be3f74c054ae279f0ca4fac2b
lucia-sanchez <sanchezyesicalucia@gmail.com>
2022-05-16 10:39:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+var respuesta = "S";
+
+while (respuesta.ToUpper() == "S"){
+    //BORRO LA PANTALLA
+Console.Clear();
+    //PIDO QUE EL USUARIO INGRESE DATOS
+Console.WriteLine("Por favor, ingrese su nombre");
+    //GUARDO EL NOMBRE EN UNA VARIABLE
+var name = Console.ReadLine();
+    //MUESTRO EL SALUDO
+Console.WriteLine($"Hola! {name}");
+    //MUESTRO LA PREGUNTA
+Console.WriteLine("¿Desea continuar?");
+    //GUARDO LA RESPUESTA EN LA MISMA VARIABLE QUE AL COMIENZO
+respuesta = Console.ReadLine();
+}
+
+if (respuesta.ToUpper()== "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+
+else{
+    Console.WriteLine("Opción no valida");
+}
+
+
+
