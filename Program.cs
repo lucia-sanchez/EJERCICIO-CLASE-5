9e672aa35b85271fc35b38259f64948a26c121e5
Lucia Sanchez <92888197+lucia-sanchez@users.noreply.github.com>
2022-05-24 16:35:04 -0300

    EJERCICIO 7

--- Program.cs
+++ Program.cs
@@ -1,30 +1,47 @@
-﻿
+﻿/*
+Escribe un programa que:
 
-var respuesta = "S";
+1) Pida 10 números al usuario.
+2) Obtener la suma de todos los números ingresados.
+3) Obtener cual es el mayor de todos los números.
+4) Obtener cual es el menor de todos los números.
+5) Obtener el promedio de todos los números.
+6) Mostrar todos los números ingresados por pantalla.
+7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.
 
-while (respuesta.ToUpper() == "S"){
-    //BORRO LA PANTALLA
-Console.Clear();
-    //PIDO QUE EL USUARIO INGRESE DATOS
-Console.WriteLine("Por favor, ingrese su nombre");
-    //GUARDO EL NOMBRE EN UNA VARIABLE
-var name = Console.ReadLine();
-    //MUESTRO EL SALUDO
-Console.WriteLine($"Hola! {name}");
-    //MUESTRO LA PREGUNTA
-Console.WriteLine("¿Desea continuar?");
-    //GUARDO LA RESPUESTA EN LA MISMA VARIABLE QUE AL COMIENZO
-respuesta = Console.ReadLine();
-}
+Recuerden que no nos deben adjuntar archivos comprimidos ni imagenes.
+Pueden adjuntarnos el archivo .cs, o el codigo como texto.
+*/
 
-if (respuesta.ToUpper()== "N")
+int [] num = new int [10];
+int acc = 0;
+int mayor = num[0];
+int menor = 2147483647;
+
+
+for (int i = 0; i < num.Length; i++)
 {
-    Console.WriteLine("Programa finalizado correctamente");
+    Console.WriteLine($"Ingrese el {i+1} número: ");
+    num[i] = int.Parse(Console.ReadLine());
+    
+ 
+    if(num[i]> mayor){
+        mayor = num[i];
+    }
+    if(num[i] < menor){
+        menor = num[i];
+    }
+    acc += num[i];
 }
 
-else{
-    Console.WriteLine("Opción no valida");
+int promedio = acc/num.Length;
+
+Console.WriteLine($"La suma de los numeros ingresados es {acc}");
+Console.WriteLine($"El mayor número ingresado es {mayor}");
+Console.WriteLine($"El menor número ingresado es {menor}");
+Console.WriteLine($"El promedio de los nùmeros ingresados es {promedio}");
+
+for (int i = 0; i < num.Length; i++)
+{
+    Console.WriteLine($"El {i +1} numero ingresado es {num[i]}");
 }
-
-
-
